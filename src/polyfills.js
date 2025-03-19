f957d18719462b1b718e53834667e5eb674c3089
thaydeveloper <thaydeveloper26@gmail.com>
2025-03-19 18:22:29 -0300

    fix: remove arquivos desnecessarios

--- src/polyfills.js
+++
@@ -1,9 +0,0 @@
-if (typeof window === 'undefined' && !global.crypto) {
-  global.crypto = {};
-}
-
-if (typeof window === 'undefined' && !global.crypto.getRandomValues) {
-  global.crypto.getRandomValues = array => {
-    return require('crypto').randomFillSync(array);
-  };
-}
